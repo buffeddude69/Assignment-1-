68dc289c4b14218249f83c6cfce22a6e9b6bc9f1
buffeddude69 <tolaybagz@gmail.com>
2026-09-01 20:00:06 +0800

    Stage 2: read endpoints with 404

--- intern-project/app/routers/tasks.py
+++ intern-project/app/routers/tasks.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+sample_database = {}
 
 @router.get("/")
 async def get_root():
@@ -11,9 +12,12 @@ async def get_root():
 @router.get("/health")
 async def get_health():
     return task_service.get_health()
-# sample_database = {}
 
-# @router.get("/tasks/{task_id}")
-# async def get_task(task_id : int):
-#     return task_service.get_task(task_id)
+@router.get("/tasks")
+async def get_tasks():
+    return task_service.get_tasks()
+
+@router.get("/tasks/{task_id}")
+async def get_task(task_id : int):
+    return task_service.get_task(task_id)
     
--- intern-project/app/services/task_service.py
+++ intern-project/app/services/task_service.py
@@ -1,5 +1,12 @@
 from app.schemas.task import CreateTask
+from fastapi.responses import JSONResponse
 
+sample_db = {
+    1: {
+        "title": "Learn FastAPI",
+        "description": "Learn layered architecture"
+    }
+}
 
 def get_root():
     return {
@@ -12,19 +19,18 @@ def get_health():
     return {
         "status" : "ok"
     }
-# sample_db = {
-#     1: {
-#         "name": "Task API",
-#         "version": "1.0",
-#         "endpoints": ["/tasks"]
-#     }
-# }
 
-# def get_task(task_id):
-#     if task_id in sample_db:
-#         return {"status" : "Found!", "task" : sample_db[task_id]}
-#     else:
-#         return {"Error!" : "Task Not Found!"}
+def get_tasks():
+    return list(sample_db.values())
+
+def get_task(task_id):
+    if task_id in sample_db:
+        return {"status" : "Found!", "task" : sample_db[task_id]}
+    else:
+        return JSONResponse(
+            status_code=404,
+            content={"error" : f"Task {task_id} not found"}
+        )
 
 
     
